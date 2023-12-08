d92de6f26b8c28a498f0a169672822fa7538a8a9
invimaticgenai <149483952+invimaticgenai@users.noreply.github.com>
2023-12-08 11:25:39 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from sentence_transformers import SentenceTransformer
+from openai import Completion
 import pinecone
 import openai
 import streamlit as st
@@ -15,16 +16,17 @@ def find_match(input):
 
 def query_refiner(conversation, query):
 
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
-    temperature=0.7,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
+    response = Completion.create(
+        model="text-davinci-003",
+        prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
-    return response['choices'][0]['text']
+
+    return response.choices[0].text
 
 def get_conversation_string():
     conversation_string = ""
