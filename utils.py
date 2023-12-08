347de536498ff8d2348e35dadc48aeeb89e3a23e
invimaticgenai <149483952+invimaticgenai@users.noreply.github.com>
2023-12-08 11:29:44 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from openai import Completion
 import pinecone
 import openai
 import streamlit as st
-openai.api_key = ""
+openai.api_key = "OPENAI_API_KEY"
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 pinecone.init(api_key='6348c0b6-eb7a-4b83-8ff9-2c2c6ab11887', environment='gcp-starter')
