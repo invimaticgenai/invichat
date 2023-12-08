bed6731d14201dad911a31cfef0e7759a019629a
invimaticgenai <149483952+invimaticgenai@users.noreply.github.com>
2023-12-08 11:01:02 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -5,8 +5,7 @@ import streamlit as st
 openai.api_key = ""
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-pinecone.init(api_key='6348c0b6-eb7a-4b83-8ff9-2c2c6ab11887', environment='	
-gcp-starter')
+pinecone.init(api_key='6348c0b6-eb7a-4b83-8ff9-2c2c6ab11887', environment='gcp-starter')
 index = pinecone.Index('invigptvector')
 
 def find_match(input):
