7853c031b136797b5aa5d053f0437ee8d023ba7c
invimaticgenai <149483952+invimaticgenai@users.noreply.github.com>
2023-12-08 11:43:37 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,22 +1,31 @@
 from sentence_transformers import SentenceTransformer
-from openai import Completion
+from openai.api import OpenAI
 import pinecone
-import openai
 import streamlit as st
-openai.api_key = "OPENAI_API_KEY"
+
+# OpenAI API key
+openai_api_key = "OPENAI_API_KEY"
+
+# Initialize OpenAI client
+openai = OpenAI(api_key=openai_api_key)
+
+# SentenceTransformer model
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
+# Pinecone configuration
 pinecone.init(api_key='6348c0b6-eb7a-4b83-8ff9-2c2c6ab11887', environment='gcp-starter')
 index = pinecone.Index('invigptvector')
+
 
 def find_match(input):
     input_em = model.encode(input).tolist()
     result = index.query(input_em, top_k=2, includeMetadata=True)
-    return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+    return (result['matches'][0]['metadata']['text'] + "\n" +
+            result['matches'][1]['metadata']['text'])
+
 
 def query_refiner(conversation, query):
-
-    response = Completion.create(
+    response = openai.Completion.create(
         model="text-davinci-003",
         prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
         temperature=0.7,
@@ -28,10 +37,11 @@ def query_refiner(conversation, query):
 
     return response.choices[0].text
 
+
 def get_conversation_string():
     conversation_string = ""
-    for i in range(len(st.session_state['responses'])-1):
-        
-        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
-        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    for i in range(len(st.session_state['responses']) - 1):
+        conversation_string += "Human: " + st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: " + st.session_state['responses'][i + 1] + "\n"
     return conversation_string
+
